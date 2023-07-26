e57e7b3020eba96e153ecc21a34dcfa099c8f136
skullbreakerr <96946235+skullbreakerr@users.noreply.github.com>
2023-07-26 16:07:38 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,66 +1,30 @@
 import * as THREE from 'three';
 import './style.css';
 
- 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight);
+const apiToken = 'f1798587198642deaa5a30a59cac7fa7';
+const modelId = 'dd37769bdc0148a988535f893d01c551';
+const url= `https://api.sketchfab.com/v3/models/${modelId}?apiKey=${apiToken}`; 
 
-const renderer = new THREE.WebGLRenderer({antialiaa:true});
-renderer.setSize(window.innerWidth,window.innerHeight);
-document.body.appendChild( renderer.domElement);
+fetch(url).then(response => response.json())
+.then(data =>{
+  const viewer = new window.SketchfabViewer({
+    client: '1.7.1',
+    iframe: document.getElementById('api-frame')
+  });
+  viewer.init(data,{ autostart:true })
+})
+.catch(error=> console.error('Error in fetching: ',error));
 
-// tut-1 (Rotating Cube) //
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight);
 
-// var geometry = new THREE.BoxGeometry(1,1,1);
-// var material = new THREE.MeshBasicMaterial({color:0xff000});
-// var cube = new THREE.Mesh(geometry,material);
-// scene.add(cube);
-// cube.position.z = -5;
-// cube.rotation.x =10;
-// cube.rotation.y =5;
-// // renderer.render(scene,camera);
+// const renderer = new THREE.WebGLRenderer({antialiaa:true});
+// renderer.setSize(window.innerWidth,window.innerHeight);
+// document.body.appendChild( renderer.domElement);
 
 // var animate = function (){
-//   cube.rotation.x+=0.01;
+//   object.rotation.x+=0.01;
 //   renderer.render(scene,camera);
 //   requestAnimationFrame(animate);
 // };
 // animate();
-
-
-// tut-2(Ring and various geometry constructor)//
-
-// var geometry = new THREE.BoxGeometry(2,2,2); // Cube
-// var geometry = new THREE.SphereGeometry(2,50,50); // Sphere
-// var geometry = new THREE.ConeGeometry(2,6,50); //Cone
-// var geometry = new THREE.OctahedronGeometry(5); //Octahedron
-// var geometry = new THREE.CylinderGeometry(2,2,4); //Cylinder
-// var geometry = new THREE.DodecahedronGeometry(2); //Dodecahedron
-
-var geometry = new THREE.TorusGeometry(2,0.2,10,100);
-// var material = new THREE.MeshPhongMaterial({color:0xff0000});
-var material = new THREE.MeshPhongMaterial({color:0xff0000,side: THREE.DoubleSide});
-
-
-var object = new THREE.Mesh(geometry,material);
-scene.add(object);
-console.log(geometry.vertices);
-
-var directionalLight = new THREE.DirectionalLight(0xffffff,1.2);
-directionalLight.position.x = 3;
-directionalLight.position.y = 4;
-directionalLight.position.z = 4;
-scene.add( directionalLight );
-
-scene.fog = new THREE.Fog(0xffffff,0,20000);
-renderer.setClearColor(scene.fog.color,1);
-object.position.z = -7;
-object.rotation.x = 10;
-object.rotation.y = 5;
-
-var animate = function (){
-  object.rotation.x+=0.01;
-  renderer.render(scene,camera);
-  requestAnimationFrame(animate);
-};
-animate();
