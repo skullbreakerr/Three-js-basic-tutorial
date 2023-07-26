4cd4831b42b7a186db4f77ae7195321ad8ae9ae0
skullbreakerr <96946235+skullbreakerr@users.noreply.github.com>
2023-07-26 17:11:10 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 import * as THREE from 'three';
 import './style.css';
+import 'https://static.sketchfab.com/api/sketchfab-viewer-1.12.1.js';
 
-const apiToken = 'f1798587198642deaa5a30a59cac7fa7';
-const modelId = 'dd37769bdc0148a988535f893d01c551';
-const url= `https://api.sketchfab.com/v3/models/${modelId}?apiKey=${apiToken}`; 
+var iframe = document.getElementById( 'api-frame' );
+var uid = 'dd37769bdc0148a988535f893d01c551';
 
-fetch(url).then(response => response.json())
-.then(data =>{
-  const viewer = new window.SketchfabViewer({
-    client: '1.7.1',
-    iframe: document.getElementById('api-frame')
-  });
-  viewer.init(data,{ autostart:true })
-})
-.catch(error=> console.error('Error in fetching: ',error));
+// By default, the latest version of the viewer API will be used.
+var client = new Sketchfab( iframe );
 
+// Alternatively, you can request a specific version.
+// var client = new Sketchfab( '1.12.1', iframe );
+
+client.init( uid, {
+    success: function onSuccess( api ){
+        api.start();
+        api.addEventListener( 'viewerready', function() {
+
+            // API is ready to use
+            // Insert your code here
+            // const scene = new THREE.Scene();
+            // const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight);
+            // const renderer = new THREE.WebGLRenderer({antialiaa:true});
+            // renderer.setSize(window.innerWidth,window.innerHeight);
+            console.log( 'Viewer is ready' );
+
+        } );
+    },
+    error: function onError() {
+        console.log( 'Viewer error' );
+    }
+} );
 // const scene = new THREE.Scene();
 // const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight);
 
